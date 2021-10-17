aa819ecff86c7aed21a83f3927ea3e476e653da3
Adna-Robotics <adna.17077@gmail.com>
2021-10-17 14:37:32 -0700

    first teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_2021.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Teleop_2021 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
