418022ccdcca6960373af00e6b0a72b2de6a0a93
Adna-Robotics <adna.17077@gmail.com>
2021-10-17 19:27:17 -0700

    added hardware class, started on 2021 teleop, added ftclib and ftc dashboard libraries

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Hardware {
+
+    public int level1   = -300;
+    public int level2   = 750;
+    public int level3   = 500;
+
+    public Motor rightFront     = null;
+    public Motor rightMiddle    = null;
+    public Motor rightBack      = null;
+    public Motor leftFront      = null;
+    public Motor leftMiddle     = null;
+    public Motor leftBack       = null;
+    public Motor lift           = null;
+    public Motor intake         = null;
+
+    public Servo intakeRight    = null;
+    public Servo intakeLeft     = null;
+    public Servo claw           = null;
+    public Servo clawAngleRight = null;
+    public Servo clawAngleLeft  = null;
+    public CRServo duckRight    = null;
+    public CRServo duckLeft     = null;
+
+    public MotorGroup rightMotors   = null;
+    public MotorGroup leftMotors    = null;
+
+    HardwareMap hardwareMap     = null;
+
+    public Hardware(){
+
+    }
+
+
+    public void initHardware(HardwareMap hardwareMap){
+
+    rightFront  = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    rightMiddle = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    rightBack   = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    leftFront   = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    leftMiddle  = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    leftBack    = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_1150);
+    lift        = new Motor(hardwareMap, "lift", Motor.GoBILDA.RPM_117);
+    intake      = new Motor(hardwareMap, "intake", Motor.GoBILDA.RPM_1150);
+
+    lift.setRunMode(Motor.RunMode.PositionControl);
+    lift.resetEncoder();
+    lift.setPositionCoefficient(0.05);
+    lift.setPositionTolerance(56);
+
+    rightMiddle.setInverted(true);
+    leftFront.setInverted(true);
+    leftBack.setInverted(true);
+
+    MotorGroup rightMotors = new MotorGroup(rightFront, rightMiddle, rightBack);
+    MotorGroup leftMotors = new MotorGroup(leftFront, leftMiddle, leftBack);
+
+    Servo intakeRight       = hardwareMap.get(Servo.class, "rightIntake");
+    Servo intakeLeft        = hardwareMap.get(Servo.class, "rightIntake");
+    Servo claw              = hardwareMap.get(Servo.class, "rightIntake");
+    Servo clawAngleRight    = hardwareMap.get(Servo.class, "rightIntake");
+    Servo clawAngleLeft     = hardwareMap.get(Servo.class, "rightIntake");
+    CRServo duckRight       = hardwareMap.get(CRServo.class, "duckRight");
+    CRServo duckLeft        = hardwareMap.get(CRServo.class, "duckLeft");
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_2021.java
@@ -1,11 +1,60 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class Teleop_2021 extends LinearOpMode {
 
+    Hardware robot = new Hardware();
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
+        robot.initHardware(hardwareMap);
 
+        while(opModeIsActive()) {
+            DifferentialDrive DT = new DifferentialDrive(robot.leftMotors, robot.rightMotors);
+
+
+
+            waitForStart();
+            DT.arcadeDrive(-gamepad1.right_stick_y, gamepad1.left_stick_x);
+
+
+
+            if(gamepad1.right_bumper){
+                robot.intake.set(1);
+            }
+            else if(gamepad1.left_bumper){
+                robot.intake.set(-1);
+            }
+            else{
+                robot.intake.set(0);
+            }
+
+
+
+            if(gamepad2.dpad_left){
+                robot.lift.setTargetPosition(robot.level1);
+            }
+            else if(gamepad2.dpad_up){
+                robot.lift.setTargetPosition(robot.level2);
+            }
+            else if(gamepad2.dpad_right){
+                robot.lift.setTargetPosition(robot.level3);
+            }
+            else if(gamepad2.dpad_down){
+                robot.lift.setTargetPosition(0);
+            }
+
+            if(!robot.lift.atTargetPosition()){
+                robot.lift.set(0.75);
+            }
+            else{
+                robot.lift.set(0);
+            }
+
+        }
     }
 }
