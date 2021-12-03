fba66077a1d973484bb2553af3aaa4dcf4acdba8
Nate Scheuber <13628@adnaschools.org>
2021-12-02 20:18:54 -0800

    Added Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Meet_2_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Meet_2_Red.java
@@ -2,7 +2,8 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.geometry.Pose2d;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -20,7 +21,7 @@ public class Meet_2_Red extends LinearOpMode {
 
         SampleTankDrive drive = new SampleTankDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(0,0, Rotation2d.fromDegrees(90));
+        Pose2d startPose = new Pose2d(0,0, Math.toRadians(90));
         drive.setPoseEstimate(startPose);
 
 
